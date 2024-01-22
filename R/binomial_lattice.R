95c214cc628da91039918bbcc64caddd6fec3801
martijer <jerko.martinic@hotmail.com>
2024-01-22 14:29:32 +0100

    update: p is now q
    update: p is now q

--- R/binomial_lattice.R
+++ R/binomial_lattice.R
@@ -1,4 +1,4 @@
-binomial_lattice <- function(n, S0, K, r, sigma = NA, T, optionType, exerciseType, u = NA, d = NA) {
+binomialLattice <- function(n, S0, K, r, sigma = NA, T, optionType, exerciseType, u = NA, d = NA) {
   #' n = Anzahl Perioden
   #' S0 = Heutige Aktienpreis
   #' K = Strike
@@ -8,7 +8,7 @@ binomial_lattice <- function(n, S0, K, r, sigma = NA, T, optionType, exerciseTyp
   #' exerciseType = Eu/Ami
   #' u = Preissprung nach oben
   #' d = Preissprung nach unten
-
+  
   #' Zeitschritte anwenden
   dt <- 1 / T
   R <- (1 + (r/T))
@@ -21,21 +21,21 @@ binomial_lattice <- function(n, S0, K, r, sigma = NA, T, optionType, exerciseTyp
   if (is.na(sigma)) {
     sigma <- log(u)*sqrt(T)
   }
-
+  
   # Risikoneutraler Zins
-  p <- (R - d) / (u - d)
-
+  q <- (R - d) / (u - d)
+  
   # Init. Matrix für stonks
   stockPrices <- matrix(0, nrow = n + 1, ncol = n + 1)
   optionValues <- matrix(0, nrow = n + 1, ncol = n + 1)
-
+  
   # Befüllen von Binomialgitter
   for (i in 0:n) {
     for (j in 0:i) {
       stockPrices[j + 1, i + 1] <- S0 * u^j * d^(i - j)
     }
   }
-
+  
   # Optionswerte bei Fälligkeit
   for (j in 0:n) {
     stockPrice <- stockPrices[j + 1, n + 1]
@@ -45,22 +45,22 @@ binomial_lattice <- function(n, S0, K, r, sigma = NA, T, optionType, exerciseTyp
       optionValues[j + 1, n + 1] <- max(K - stockPrice, 0)
     }
   }
-
+  
   # Derivatpreis Rückwärts rechnen
   for (i in (n - 1):0) {
     for (j in 0:i) {
-      EUValue <- (1/R) * (((p * optionValues[j + 2, i + 2]) + ((1 - p) * optionValues[j + 1, i + 2])))
+      EUValue <- (1/R) * (((q * optionValues[j + 2, i + 2]) + ((1 - q) * optionValues[j + 1, i + 2])))
       if (exerciseType == "American") {
         stockPrice <- stockPrices[j + 1, i + 1]
         intrinsicValue <- if (optionType == "Call") max(stockPrice - K, 0) else max(K - stockPrice, 0)
-        if (intrinsicValue > EUValue) print(c(i+1,j+1))#paste("Vorz. Ausübung Lohnt sich bei Node: ",c(i+1,j+1))
+        if (intrinsicValue > EUValue) print(c(j+1,i+1))#paste("Vorz. Ausübung Lohnt sich bei Node: ",c(i+1,j+1))
         optionValues[j + 1, i + 1] <- max(EUValue, intrinsicValue)
       } else {
         optionValues[j + 1, i + 1] <- EUValue
       }
     }
   }
-
-
-  return(list(R=R, dt=dt, u=u, d=d, p=p, sigma=sigma, assetlattice = stockPrices, optionValues = optionValues))
+  
+  
+  return(list(R=R, dt=dt, u=u, d=d, q=q, sigma=sigma, assetlattice = stockPrices, optionValues = optionValues))
 }
